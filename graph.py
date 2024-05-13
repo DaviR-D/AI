c98d5a11c939588ac8e9f2317bfbcd24da06461d
Davi-RD <davirdavanzo@gmail.com>
2024-05-12 21:49:02 -0300

    Correção nome de cidade

--- graph.py
+++ graph.py
@@ -4,7 +4,7 @@ def connectNodes(n1, n2, weight):
     n1.addConection(n2, weight)
     n2.addConection(n1, weight)
 
-cities = ['Oraclea',
+cities = ['Oradea',
            'Zerind',
            'Arad',
            'Timisoara',
@@ -30,14 +30,14 @@ graph = dict()
 for city in cities:
     graph[city] = Node(city)
     
-connectNodes(graph['Oraclea'], graph['Zerind'], 71)
+connectNodes(graph['Oradea'], graph['Zerind'], 71)
 connectNodes(graph['Zerind'], graph['Arad'], 75)
 connectNodes(graph['Arad'], graph['Timisoara'], 118)
 connectNodes(graph['Timisoara'], graph['Lugoj'], 111)
 connectNodes(graph['Lugoj'], graph['Mehadia'], 70)
 connectNodes(graph['Mehadia'], graph['Dobreta'], 75)
 connectNodes(graph['Dobreta'], graph['Craiova'], 120)
-connectNodes(graph['Sibiu'], graph['Oraclea'], 151)
+connectNodes(graph['Sibiu'], graph['Oradea'], 151)
 connectNodes(graph['Sibiu'], graph['Arad'], 140)
 connectNodes(graph['Sibiu'], graph['Fagaras'], 99)
 connectNodes(graph['Sibiu'], graph['Rimnicu Vilcea'], 80)
