307526ae83cca314733272d5137ba5d76099b439
Davi-RD <davirdavanzo@gmail.com>
2024-06-11 18:36:30 -0300

    Implementação do DSF comentado a exibição de caminho no BSF

--- torre_hanoi.py
+++ torre_hanoi.py
@@ -21,27 +21,47 @@ def generate_next_states(state, visited_states):
 def bfs(initial_state, final_state):
     visited_states = []
     queue = [initial_state]
-    predecessors = {tuple(map(tuple, initial_state)): None}
+    #predecessors = {tuple(map(tuple, initial_state)): None}
 
     while queue:
         state = queue.pop(0)
         visited_states.append(state)
+        print(state)
         if state == final_state:
-            path = []
-            while state is not None:
-                path.append(state)
-                state = predecessors[tuple(map(tuple, state))]
-            path.reverse()
-            for step in path:
-                print(step)
+        #     path = []
+        #     while state is not None:
+        #         path.append(state)
+        #         state = predecessors[tuple(map(tuple, state))]
+        #     path.reverse()
+        #     for step in path:
+        #         print(step)
             return
         for next_state in generate_next_states(state, visited_states):
             if(next_state not in visited_states):
                 queue.append(next_state)
-                predecessors[tuple(map(tuple, next_state))] = state
+                #predecessors[tuple(map(tuple, next_state))] = state
+
+
+def dfs(initial_state, final_state):
+    visited_states = []
+    stack = [initial_state]
+
+    while stack:
+        state = stack.pop()
+        visited_states.append(state)
+        print(state)
+        if(state == final_state):
+            return
+        for next_state in generate_next_states(state, visited_states):
+            if next_state not in visited_states:
+                visited_states.append(next_state)
+                stack.append(next_state)
+            
+
+
 
 initial_state = [[5,4,3,2,1], [], []]
 
 final_state = [[], [], [5,4,3,2,1]]
 
-bfs(initial_state, final_state)
+dfs(initial_state, final_state)
